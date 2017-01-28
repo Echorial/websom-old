e1e012fdbedb50366109c8e340ac0d89cbedd7b9
AskingQuestions <tiger11451145@gmail.com>
2017-01-28 15:08:00 -0500

    Added javascript docs and ClientTools
    See docs for more info.

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,10 +1,16 @@
-
 $(document).ready(function () {
-
 	CallEventHook("theme", [function () {
 		CallEventHook("themeReady");
 	}]);
-
 });
 
 window.Websom = {};
+
+$(document).on("click", ":not(.ws_not_safe) .ws_submit_form, :not(.ws_not_safe) .ws_click_element", function () {
+	var that = $(this);
+	if (that.hasAttr("data-ws-form")) {
+		$("[name=global__forms__"+that.attr("data-ws-form")+"]").trigger("submit");
+	}else if(that.hasAttr("data-ws-element")){
+		$(that.attr("data-ws-element")).trigger("click");
+	}
+});
