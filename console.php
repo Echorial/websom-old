004c322d6830c392d6533a23766ca5bcf02dda7f
AskingQuestions <tiger11451145@gmail.com>
2017-03-06 17:09:18 -0500

    Added suppress modules option to console.php
    You can now append ?no_suppress to the console.php url.

--- console.php
+++ console.php
@@ -1,6 +1,7 @@
 <?php
 define("Console_Page", true);
-define("Suppress_Modules", true);
+if (!isset($_GET["no_suppress"]))
+	define("Suppress_Modules", true);
 include("Websom/Start.php");
 Page("blank.html");
 
